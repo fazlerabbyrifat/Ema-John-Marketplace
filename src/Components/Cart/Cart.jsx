68d1498ad8704388f583ee0e393e24df1685830d
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-03-28 17:47:37 +0600

    show data from cart

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -6,10 +6,13 @@ const Cart = ({cart}) => {
 
     let totalPrice = 0;
     let totalShipping = 0
+    let quantity = 0;
 
     for (const product of cart){
-        totalPrice = totalPrice + product.price;
-        totalShipping = totalShipping + product.shipping;
+        product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
 
     const tax = totalPrice * 9 / 100;
@@ -19,7 +22,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
